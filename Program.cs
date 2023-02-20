b1962b86adf999ddc6f94d91d31b57821ca822c2
SoulWater58 <99378089+SoulWater58@users.noreply.github.com>
2023-02-20 12:19:37 +0900

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Lab8ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"Число {num} в квадрате = {num * num}");
+        }
+    }
+}
